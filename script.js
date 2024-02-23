9548c5a5e2508cf3ad0a08ec0dfcd75d62ca4bf3
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2024-02-23 19:05:13 +0900

    refactor + added support for v1 and v3 contracts... there's work to do

--- script.js
+++ script.js
@@ -1,54 +1,37 @@
-import { ethers } from "./ethers-5.6.esm.min.js"
-import { abiV1, contractAddressV1 } from "./abV1.js"
-import { abiV2, contractAddressV2 } from "./abV2.js"
-import { abiV3, contractAddressV3 } from "./abV3.js"
+import { ethers } from "./constants/ethers-5.6.esm.min.js"
+import { abiV1, contractAddressV1 } from "./constants/abV1.js"
+import { abiV2, contractAddressV2 } from "./constants/abV2.js"
+import { abiV3, contractAddressV3 } from "./constants/abV3.js"
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://eth.llamarpc.com"
 )
-const contractV1 = new ethers.Contract(
-  contractAddressV1,
-  abiV1,
-  provider
-) /** first tokenID 0 */
-const contractV2 = new ethers.Contract(
-  contractAddressV2,
-  abiV2,
-  provider
-) /** first tokenID 3000000 */
-const contractV3 = new ethers.Contract(
-  contractAddressV3,
-  abiV3,
-  provider
-) /** first tokenID 374000000 */
+const contracts = [
+  { abi: abiV1, address: contractAddressV1 },
+  { abi: abiV2, address: contractAddressV2 },
+  { abi: abiV3, address: contractAddressV3 },
+].map(({ abi, address }) => new ethers.Contract(address, abi, provider))
 
 const tokenIdInput = document.getElementById("tokenId")
 const showButton = document.getElementById("showButton")
-let tknData = document.getElementById("tknData")
-let artCode = document.getElementById("artCode")
+const tknData = document.getElementById("tknData")
+const artCode = document.getElementById("artCode")
 
 let _tokenId = ""
 let _hash = ""
-let _script
+let _script = ""
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  const storedTokenId = localStorage.getItem("_tokenId")
-  const storedHash = localStorage.getItem("_hash")
-  const storedScript = localStorage.getItem("_script")
-
-  if (storedTokenId && storedHash && storedScript) {
-    _tokenId = storedTokenId
-    _hash = storedHash
-    _script = storedScript
+window.addEventListener("DOMContentLoaded", () => {
+  const storedData = JSON.parse(localStorage.getItem("tokenData"))
+  if (storedData) {
+    ;({ _tokenId, _hash, _script } = storedData)
     updateTknData(_tokenId, _hash)
     updateArtCode(_script)
   }
 })
 
 function clearLocalStorage() {
-  localStorage.removeItem("_tokenId")
-  localStorage.removeItem("_hash")
-  localStorage.removeItem("_script")
+  localStorage.removeItem("tokenData")
 }
 
 async function show() {
@@ -56,48 +39,59 @@ async function show() {
   try {
     clearLocalStorage()
 
-    _hash = await contractV2.tokenIdToHash(_tokenId)
-    let projId = await contractV2.tokenIdToProjectId(_tokenId)
-    let projectInfo = await contractV2.projectScriptInfo(projId)
-
-    _script = ""
-    for (let i = 0; i < projectInfo[1].toNumber(); i++) {
-      const scrpt = await contractV2.projectScriptByIndex(projId.toString(), i)
-      _script += scrpt
+    let contractIndex = 0
+    if (_tokenId >= 3000000 && _tokenId < 374000000) {
+      contractIndex = 1 // v2 contract
+    } else if (_tokenId >= 374000000) {
+      contractIndex = 2 // v3 contract
     }
 
-    // console.log("token Id is:", _tokenId)
-    // console.log("Hash is:", _hash)
-    // console.log("Project ID is:", projId.toString())
-    // console.log("Script count is:", projectInfo[1].toNumber())
-    // console.log("Script is:", _script)
+    const contractToUse = contracts[contractIndex]
+    _hash = await (contractIndex === 0
+      ? contractToUse.showTokenHashes(_tokenId)
+      : contractToUse.tokenIdToHash(_tokenId))
+
+    const projId = await contractToUse.tokenIdToProjectId(_tokenId)
+    const projectInfo = await (contractIndex === 2
+      ? contractToUse.projectScriptDetails(projId.toString())
+      : contractToUse.projectScriptInfo(projId.toString()))
+
+    _script = ""
+    for (
+      let i = 0;
+      i <
+      (contractIndex === 2
+        ? projectInfo[2].toNumber()
+        : projectInfo[1].toNumber());
+      i++
+    ) {
+      const scrpt = await contractToUse.projectScriptByIndex(
+        projId.toString(),
+        i
+      )
+      _script += scrpt
+    }
 
     updateTknData(_tokenId, _hash)
     updateArtCode(_script)
 
-    localStorage.setItem("_tokenId", _tokenId)
-    localStorage.setItem("_hash", _hash)
-    localStorage.setItem("_script", _script)
-
-    window.location.reload()
+    localStorage.setItem(
+      "tokenData",
+      JSON.stringify({ _tokenId, _hash, _script })
+    )
+    location.reload()
   } catch (error) {
     console.error("Error:", error)
   }
 }
 
 function updateTknData(_tokenId, _hash) {
-  let tokenDataObj = {
-    tokenId: _tokenId,
-    hash: _hash,
-  }
-  tknData.innerText = "let tokenData = " + JSON.stringify(tokenDataObj)
-  eval(tknData.innerText)
+  tknData.innerText = `let tokenData = { tokenId: "${_tokenId}", hash: "${_hash}" }`
   console.log(tknData.innerText)
 }
 
 function updateArtCode(_script) {
   artCode.innerText = _script
-  eval(artCode.innerText)
   console.log(artCode.innerText)
 }
 
