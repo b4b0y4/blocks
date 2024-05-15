cc8f1d129540e63c13b38ac39e27cf79e8a66820
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2024-05-15 12:10:16 +0900

    rename codeLib to externalLib

--- script.js
+++ script.js
@@ -797,7 +797,7 @@ async function grabData(tokenId, contract) {
     const script = await constructScript(projId, projectInfo, contract)
     const detail = await fetchProjectDetails(projId, contract)
     const owner = await fetchOwner(tokenId, contract)
-    const codeLib = extractLibraryName(projectInfo)
+    const externalLib = extractLibraryName(projectInfo)
     const { edition, remaining } = await fetchEditionInfo(
       projId,
       contract,
@@ -810,7 +810,7 @@ async function grabData(tokenId, contract) {
       script,
       detail,
       owner,
-      codeLib,
+      externalLib,
       edition,
       remaining,
     })
@@ -890,12 +890,12 @@ function update(
   script,
   detail,
   owner,
-  codeLib,
+  externalLib,
   edition,
   remaining
 ) {
   // Update library source
-  localStorage.setItem("Src", predefinedLibraries[codeLib])
+  localStorage.setItem("Src", predefinedLibraries[externalLib])
 
   // Update tokenIdHash content
   const tknData =
@@ -907,7 +907,7 @@ function update(
 
   // Update artCode
   let process = ""
-  if (codeLib === "processing") {
+  if (externalLib === "processing") {
     process = "application/processing"
   }
   localStorage.setItem("Type", process)
@@ -1066,7 +1066,7 @@ async function injectFrame() {
     </body>`
 
     let dynamicContent
-    if (storedData.codeLib === "custom") {
+    if (storedData.externalLib === "custom") {
       dynamicContent = `<script>${frameIdHash}</script>${frameArt}`
     } else {
       dynamicContent = `<html>${frameHead}${frameBody}</html>`
@@ -1346,7 +1346,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // console.log("Id an Hash:", localStorage.getItem("IdHash"))
   // console.log("code type:", localStorage.getItem("Type"))
   // console.log("Art script:", localStorage.getItem("Art"))
-  // console.log("library:", storedData.codeLib)
+  // console.log("library:", storedData.externalLib)
 })
 
 rpcUrlInput.addEventListener("keypress", (event) => {
