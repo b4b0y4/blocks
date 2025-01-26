199e44fa2edb5a48cc825d91e53a5fdf87650bfd
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-01-26 10:18:28 +0100

    list update

--- js/lists.js
+++ js/lists.js
@@ -506,7 +506,7 @@ export const list = [
   "ABSIX2 - Framework / Jeff Davis - 10 items",
   "ABSX0 - Cosmicodes / Insigħt × Leonardo Petrucci - 14 items",
   "ABSXI0 - Alchimie / RVig - 80 items",
-  "ABSXII0 - tied up, tied down, let loose, unwound / Ben Snell - 14 items",
+  "ABSXII0 - tied up, tied down, let loose, unwound / Ben Snell - 17 items",
   "ABSXIII0 - AnnoMetta / Matto - 1 item",
   "ABSXV0 - Untitled / Olga Fradina - 33 items",
   "ABSXVI0 - Incircles / Jos Vromans - 64 items",
