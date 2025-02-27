afb83a1221a6553c41f4a48a750fa0f29d5e61ed
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-02-27 11:43:32 +0100

    .

--- js/script.js
+++ js/script.js
@@ -44,19 +44,15 @@ checkForNewContracts();
 async function fetchBlocks(array) {
   await new Promise((resolve) => setTimeout(resolve, 100));
   console.log("%cLOOKING FOR BLOCKS...", "color: crimson;");
-
   for (const contractName of array) {
     const n = indexMap[contractName];
     const start = contractsData[contractName].startProjId || 0;
     const end = Number(await instance[n].nextProjectId());
     const results = [];
-
     const batchSize = 5;
-
     for (let batchStart = start; batchStart < end; batchStart += batchSize) {
       const batchEnd = Math.min(batchStart + batchSize, end);
       const batchPromises = [];
-
       for (let i = batchStart; i < batchEnd; i++) {
         batchPromises.push(
           (async (id) => {
@@ -66,21 +62,15 @@ async function fetchBlocks(array) {
                 ? instance[n].projectTokenInfo(id)
                 : instance[n].projectStateData(id),
             ]);
-
-            const minted = Number(token.invocations) === 1 ? "item" : "items";
-            const newItem = `${contractName}${id} - ${detail[0]} / ${detail[1]} - ${token.invocations} ${minted}`;
-
+            const newItem = `${contractName}${id} - ${detail[0]} / ${detail[1]} - ${token.invocations} ${Number(token.invocations) === 1 ? "item" : "items"}`;
             return !list.includes(newItem) ? `"${newItem}",` : null;
           })(i),
         );
       }
-
       results.push(...(await Promise.all(batchPromises)).filter(Boolean));
     }
-
     if (results.length > 0) console.log(results.join("\n"));
   }
-
   console.log("%cDONE!!!", "color: lime;");
 }
 
@@ -89,7 +79,6 @@ function checkForNewContracts() {
   const newContract = Object.keys(contractsData).filter(
     (key) => !existingContracts.has(key),
   );
-
   if (newContract.length > 0) {
     fetchBlocks(newContract);
   }
