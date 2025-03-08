49a5f7fb508616067b828a1670d719b383bf882c
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-03-08 16:44:22 +0100

    .

--- js/script.js
+++ js/script.js
@@ -68,49 +68,6 @@ let loopState = JSON.parse(localStorage.getItem("loopState")) || {
 /**********************************************************
  *                UPDATE LIST FUNCTION
  *********************************************************/
-// async function fetchBlocks(array) {
-//   await new Promise((resolve) => setTimeout(resolve, 100));
-//   console.log("%cLOOKING FOR BLOCKS...", "color: crimson;");
-
-//   for (const contractName of array) {
-//     const n = indexMap[contractName];
-//     const start = contractRegistry[contractName].startProjId || 0;
-//     const end = Number(await instance[n].nextProjectId());
-//     const results = [];
-//     const batchSize = 5;
-
-//     for (let batchStart = start; batchStart < end; batchStart += batchSize) {
-//       const batchEnd = Math.min(batchStart + batchSize, end);
-//       const batchPromises = [];
-
-//       for (let i = batchStart; i < batchEnd; i++) {
-//         batchPromises.push(
-//           (async (id) => {
-//             const [detail, token] = await Promise.all([
-//               instance[n].projectDetails(id.toString()),
-//               is.v2.includes(contractName)
-//                 ? instance[n].projectTokenInfo(id)
-//                 : instance[n].projectStateData(id),
-//             ]);
-
-//             const newItem = `${contractName}${id} - ${detail[0]} / ${detail[1]} - ${token.invocations} ${
-//               Number(token.invocations) === 1 ? "item" : "items"
-//             }`;
-
-//             return !list.includes(newItem) ? `"${newItem}",` : null;
-//           })(i),
-//         );
-//       }
-
-//       results.push(...(await Promise.all(batchPromises)).filter(Boolean));
-//     }
-
-//     if (results.length > 0) console.log(results.join("\n"));
-//   }
-
-//   console.log("%cDONE!!!", "color: lime;");
-// }
-
 async function fetchBlocks(array) {
   await new Promise((resolve) => setTimeout(resolve, 100));
   console.log("%cLOOKING FOR BLOCKS...", "color: crimson;");
