38198b7880fb2ce108e62f3293f5d83069b21fa9
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-03-22 09:08:50 +0100

    .

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import { ethers } from "./ethers.min.js";
 import { list, libs } from "./lists.js";
 import { contractRegistry, is } from "./contracts.js";
 
-fetchBlocks(["ABC", ...is.studio]);
+// fetchBlocks(["ABC", ...is.studio]);
 
 const dom = {
   root: document.documentElement,
