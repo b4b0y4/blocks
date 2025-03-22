87671f8a8cf5bd39bbf8fce364dba28bedb5de24
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-03-22 08:29:41 +0100

    list update

--- js/lists.js
+++ js/lists.js
@@ -493,7 +493,7 @@ export const list = [
   "ABSVII0 - Deco Echo / Harvey Rayner - 300 items",
   "ABSIX0 - On-Chain Memory Token / Jeff Davis - 100 items",
   "ABSIX2 - Framework / Jeff Davis - 13 items",
-  "ABSIX3 - Progression / Jeff Davis - 434 items",
+  "ABSIX3 - Progression / Jeff Davis - 439 items",
   "ABSX0 - Cosmicodes / Insigħt × Leonardo Petrucci - 19 items",
   "ABSXI0 - Alchimie / RVig - 80 items",
   "ABSXII0 - tied up, tied down, let loose, unwound / Ben Snell - 101 items",
