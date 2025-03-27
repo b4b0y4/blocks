de452c743d27c91397633e0a55bbc256e729b8a1
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-03-27 09:50:52 +0100

    revert

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
+const CACHE_NAME = "blocks-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/script.js",
+  "./js/ethers.min.js",
+  "./js/lists.js",
+  "./js/contracts.js",
+  "./js/abis.js",
+  "./img/favicon-32x32.png",
+  "./img/icon-192x192.png",
+  "./img/icon-512x512.png",
+  "./img/apple-touch-icon.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css",
+];
+
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()),
+  );
 });
 
 self.addEventListener("activate", (event) => {
@@ -7,7 +30,10 @@ self.addEventListener("activate", (event) => {
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames
-          .filter((cacheName) => cacheName.startsWith("blocks-cache-"))
+          .filter(
+            (cacheName) =>
+              cacheName.startsWith("blocks-cache-") && cacheName !== CACHE_NAME,
+          )
           .map((cacheName) => caches.delete(cacheName)),
       );
     }),
@@ -15,5 +41,9 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request)),
+  );
 });
