d4c4f8a340b93a9b5ad89a50700484f086f0ae30
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-03-27 09:54:28 +0100

    simple sw

--- sw.js
+++ sw.js
@@ -2,18 +2,6 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName.startsWith("blocks-cache-"))
-          .map((cacheName) => caches.delete(cacheName)),
-      );
-    }),
-  );
-});
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
