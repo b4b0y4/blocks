0fc65bbfcdb60394e853a15510f5b30cf98d66fc
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-06-25 21:58:35 +0200

    list update

--- js/lists.js
+++ js/lists.js
@@ -509,7 +509,7 @@ export const list = [
   "ABSI1 # Three More Days / Aaron Penne # 1 Work",
   "ABSII0 # variaciones del yo / Marcelo Soria-Rodríguez # 54 Works",
   "ABSIII0 # Fragmented Perception / Motus Art # 25 Works",
-  "ABSIII1 # Contours / Motus Art # 1 Work",
+  "ABSIII1 # Contours / Motus Art # 5 Works",
   "ABSIV0 # Monochronos / Heeey # 40 Works",
   "ABSVII0 # Deco Echo / Harvey Rayner # 300 Works",
   "ABSIX0 # On-Chain Memory Token / Jeff Davis # 100 Works",
