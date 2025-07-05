ef37ae6d1dca68f134f06d255b0374e3c5f6b02e
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-07-05 08:12:16 +0200

    list update

--- js/lists.js
+++ js/lists.js
@@ -871,7 +871,7 @@ export const list = [
   "NEWRAFAEL32 # Zoom / Rafaël Rozendaal # 36 Works",
   "NEWRAFAEL33 # Balance / Rafaël Rozendaal # 24 Works",
   "NEWRAFAEL34 # Ripple / Rafaël Rozendaal # 30 Works",
-  "NEWRAFAEL35 # House / Rafaël Rozendaal # 19 Works",
+  "NEWRAFAEL35 # House / Rafaël Rozendaal # 22 Works",
   "NEWRAFAEL36 # Pulse / Rafaël Rozendaal # 21 Works",
   "NEWRAFAEL37 # Grit / Rafaël Rozendaal # 1 Work",
   "XCORE0 # ICXN / XCOPY # 500 Works",
