582b87e5ddf3873cfb64007455963793b25af15c
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-08-10 09:46:21 +0200

    list update

--- js/lists.js
+++ js/lists.js
@@ -546,7 +546,7 @@ export const list = [
   "ABSXXXI0 # Chroma Genesis / xnmtrc # 46 Works",
   "ABSXXXII0 # And Yet We Dance / Ryan Bell # 100 Works",
   "ABSXXXV0 # Songs of Utopia / KALA # 9 Works",
-  "ABSXXXVIII0 # 80s Pop Roxy Bonus Pack 🍿 / Daniel Catt # 58 Works",
+  "ABSXXXVIII0 # 80s Pop Roxy Bonus Pack 🍿 / Daniel Catt # 59 Works",
   "ABSLIII0 # E-PASTEL II / Holger Lippmann # 71 Works",
   "ABSLIV0 # Lightweight Reconstruction / itsgalo x office ca # 128 Works",
   "ABSLVI0 # I. / ryley-o.eth # 240 Works",
