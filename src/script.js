d4eecb82468a68d31eda5dd8105e10126021e745
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-09-02 11:17:00 +0200

    Add viewport meta tag to injected frame

--- src/script.js
+++ src/script.js
@@ -891,6 +891,7 @@ async function injectFrame() {
       : `<!DOCTYPE html>
         <html>
           <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
             ${scriptTags}
             <script>${tokenIdHash};</script>
             <style>${styles}</style>
