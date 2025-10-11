848b76b4d7115bb4e6ebc4c8df5d6efc6d796c99
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2025-10-11 10:33:20 +0200

    update list

--- js/data.js
+++ js/data.js
@@ -573,7 +573,7 @@ export const list = [
   "ABSLXXXV0 # The light where we meet / Amy Goodchild x The Generative Art Museum # 19 Works",
   "ABSLXXXVI0 # VIVOTECA / bustavo x The Generative Art Museum # 6 Works",
   "ABSLXXXVII0 # Transformations du Champ / HAL09999 x The Generative Art Museum # 11 Works",
-  "ABSLXXXVIII0 # Broken Dreams / Manuel Larino x The Generative Art Museum # 48 Works",
+  "ABSLXXXVIII0 # Broken Dreams / Manuel Larino x The Generative Art Museum # 49 Works",
   "ABEXP0 # Friendship Bracelets / Alexis André # 38758 Works",
   "ABEXP1 # Marfa Yucca / Daniel Calderon Arenas # 390 Works",
   "ABEXP2 # marfaMESH / Harvey Rayner # 343 Works",
