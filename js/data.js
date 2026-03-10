d45cfc2c0961284f2e6cc174b0ff189b9c947c3f
b4b0y4 <133552984+b4b0y4@users.noreply.github.com>
2026-03-10 08:26:54 +0100

    list update

--- js/data.js
+++ js/data.js
@@ -517,7 +517,7 @@ export const list = [
   "ABSVII1 # Amplitudes of Canvas / Harvey Rayner # 555 Works",
   "ABSVII2 # Covehithe / Harvey Rayner # 60 Works",
   "ABSVII3 # Algorithmic Synesthesia CC / Harvey Rayner # 0 Works!",
-  "ABSVII4 # Algorithmic Synesthesia AC / Harvey Rayner # 0 Works!",
+  "ABSVII4 # Algorithmic Synesthesia AC / Harvey Rayner # 12 Works",
   "ABSVIII0 # Edge of Space / Robert Baker # 100 Works",
   "ABSIX0 # On-Chain Memory Token / Jeff Davis # 200 Works",
   "ABSIX1 # Untitled / Jeff Davis # 1 Work!",
